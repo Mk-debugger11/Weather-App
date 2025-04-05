978202a3a6578becc272a3157c60a7041fb21810
Mukul Kumar <mukulkumarsharma540@gmail.com>
2025-04-06 01:22:13 +0530

    updated ui

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ let temp3 = document.getElementById('temp3');
 let description = document.getElementById('weather-desc-text')
 let place = document.querySelector('.place-text')
 let date = document.querySelector('.date-text')
-searchButton.addEventListener('click',(event)=>{
+let aqiValue = document.querySelector('.aqi-text-value')
+let humidityValue = document.querySelector('.humidity-value')
+let pressureValue = document.querySelector('.pressure-value')
+let df1 = document.querySelector('.df1')
+let df2 = document.querySelector('.df2')
+let df3 = document.querySelector('.df3')
+let df4 = document.querySelector('.df4')
+let tf1 = document.querySelector('.tf1')
+let tf2 = document.querySelector('.tf2')
+let tf3 = document.querySelector('.tf3')
+let tf4 = document.querySelector('.tf4')
+searchButton.addEventListener('click',(event)=>{ 
     event.preventDefault();  //when the submit button is clicked the event related to a button occurs
     let cityName = city.value;
     //latitude and longitude api
@@ -25,7 +36,7 @@ searchButton.addEventListener('click',(event)=>{
         let country = weatherData.sys.country
         let timeStamp = weatherData.dt
         let time = new Date(timeStamp * 1000)
-        let day = time.toLocaleDateString("en-US",{weekday:'long'})
+        let day = time.toLocaleDateString("en-US",{day:'numeric',month:'short', weekday:'short'})
         console.log(country)
         console.log(descriptionText);
         temp.innerText = `${temperature}`
@@ -34,6 +45,44 @@ searchButton.addEventListener('click',(event)=>{
         description.innerHTML = `<span>${descriptionText}</span>`;
         place.innerText = `${cityName},${country}`;
         date.innerText = `${day}`
+        let humidity = weatherData.main.humidity
+        humidityValue.innerText = `${humidity} %`
+        let pressure = weatherData.main.pressure
+        pressureValue.innerText = `${pressure} hPa`
         console.log(weatherData);
+        let lat = weatherData.coord.lat
+        let lon = weatherData.coord.lon
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+        .then((data2)=>{return data2.json()})
+        .then((forecastData)=>{
+            let requiredData = forecastData.list.filter((ele)=>{return ele.dt_txt.includes('12:00:00')})
+            let dateArray = requiredData.map((ele)=>{
+                let timeS = ele.dt;
+                let timeF = new Date(timeS * 1000);
+                let dayF = timeF.toLocaleDateString("en-US",{day:'numeric',month:'short'})
+                return dayF
+            })
+            console.log(dateArray)
+            let tempArr = requiredData.map((ele)=>{
+                return Math.floor(ele.main.temp)
+            })
+            console.log(tempArr)
+            df1.innerText = `${dateArray[1]}`
+            df2.innerText = `${dateArray[2]}`
+            df3.innerText = `${dateArray[3]}`
+            df4.innerText = `${dateArray[4]}`
+            tf1.innerText = `${tempArr[1]}°C`
+            tf2.innerText = `${tempArr[2]}°C`
+            tf3.innerText = `${tempArr[3]}°C`
+            tf4.innerText = `${tempArr[4]}°C`
+
+        })
+        fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        .then((data1)=>{return data1.json()})
+        .then((aqiData)=>{
+            let pm2_5 = aqiData.list[0].components.pm2_5
+            aqiValue.innerText = `${pm2_5} µg/m³`
+        });
+        
     })
 })
